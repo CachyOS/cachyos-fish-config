765afd8c2761fdb1093d2ff850fc2950330a4575
Peter Jung <admin@ptr1337.dev>
2025-06-30 20:10:44 +0200

    cachyos-config: Remove please alias
    Signed-off-by: Peter Jung <admin@ptr1337.dev>

--- cachyos-config.fish
+++ cachyos-config.fish
@@ -111,7 +111,6 @@ alias mirror="sudo cachyos-rate-mirrors"
 # Help people new to Arch
 alias apt='man pacman'
 alias apt-get='man pacman'
-alias please='sudo'
 alias tb='nc termbin.com 9999'
 
 # Cleanup orphaned packages
